ae2538b43c6d88f58a98d20db327f2c6f9d5c736
soham6540 <savitafarkade6540@gmail.com>
2024-10-08 00:39:58 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+
+
+
 # (©)CodeXBotz
 
 import os
@@ -24,44 +27,15 @@ PORT = os.environ.get("PORT", "8080")
 
 # Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://vikas:vikas@vikas.yfezexk.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "sharexbotjjkverse")
+DB_NAME = os.environ.get("DATABASE_NAME", "exbot")
 
 # Number of workers for the bot
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-# Force subscribe channel
-FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "-1002109545727")  # Add your channel ID here
+# User reply text
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
+# Bot stats text
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-# Messages
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}...")
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}...")
 
-try:
-    ADMINS = [int(x) for x in os.environ.get("ADMINS", "").split() if x.isdigit()]
-except ValueError:
-    raise Exception("Your Admins list does not contain valid integers.")
-
-# Other settings
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
-PROTECT_CONTENT = os.environ.get('PROTECT_CONTENT', "False") == "True"
-DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
-
-ADMINS.append(OWNER_ID)
-
-LOG_FILE_NAME = "filesharingbot.txt"
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
-    datefmt='%d-%b-%y %H:%M:%S',
-    handlers=[
-        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
-        logging.StreamHandler()
-    ]
-)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-def LOGGER(name: str) -> logging.Logger:
-    return logging.getLogger(name)
-
+# Other settings...
